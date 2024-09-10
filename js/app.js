49578f2b366b311b44c19a361af59293305f981d
BOTI <>
2024-09-10 13:28:45 +0200

    kész
    Kész

--- js/app.js
+++ js/app.js
@@ -10,23 +10,23 @@
 
 const makeBoxes = () => {
     const data = [
-        {number: 1},
-        {number: 2},
-        {number: 3},
-        {number: 4},
-        {number: 5},
-        {number: 6},
-        {number: 7},
-        {number: 8},
-        {number: 9},
-        {number: 10},
-        {number: 11},
-        {number: 12},
-        {number: 13},
-        {number: 14},
-        {number: 15}
+        { number: 1 },
+        { number: 2 },
+        { number: 3 },
+        { number: 4 },
+        { number: 5 },
+        { number: 6 },
+        { number: 7 },
+        { number: 8 },
+        { number: 9 },
+        { number: 10 },
+        { number: 11 },
+        { number: 12 },
+        { number: 13 },
+        { number: 14 },
+        { number: 15 }
     ];
-    const content = data.map(({number}) => `<div id="${number}" class="box">${number}</div>`);
+    const content = data.map(({ number }) => `<div id="${number}" class="box">${number}</div>`);
     return content;
 }
 
@@ -36,20 +36,96 @@ const renderBoxes = () => {
     boxesContainer.innerHTML = "";
     boxesContainer.innerHTML = makeBoxes().join('');
 
-   /* const boxes = document.querySelectorAll(".box");
-    console.log(boxes);*/
+    /* const boxes = document.querySelectorAll(".box");
+     console.log(boxes);*/
 };
 
 // Oldal betöltésekor inicializálódik a weboldal:
 /*document.addEventListener("DOMContentLoaded", renderBoxes);
 
 const boxes = document.querySelectorAll(".box");
-console.log(boxes);*/ 
+console.log(boxes);*/
+
+
+
+const getInputValue = function () {
+    return document.querySelector("#num").value;
+}
+
+const checkValue = () => {
+    const value = getInputValue();
+    if (!value.trim()) {
+        return [false, 0];
+    }
+    if (isNaN(value)) {
+        return [false, 0];
+    }
+    const currentValue = Number(value);
+    if (currentValue < 1 || currentValue > 15) {
+        return [false, 0];
+    }
+    return [true, currentValue];
+}
+
+
+const randomNumber = () => {
+    return Math.floor(Math.random() * 256);
+}
+
+
+const createRGBColor = () => {
+    const r = randomNumber();
+    const g = randomNumber();
+    const b = randomNumber();
+    return [r, g, b];
+
+}
+
+const coloringBox = () => {
+    const [isValid, number] = checkValue();
+    if (!isValid) {
+        sendErrorMessage();
+        return;
+    }
+    const boxes = document.querySelectorAll(".box")
+    const box = Array.from(boxes).find(b => Number(b.id) === number);
+    const [r, g, b] = createRGBColor();
+    box.style.backgroundColor = `rgb(${r}, ${g}, ${b})`
+}
+
+
+function sendErrorMessage() {
+    alert("Helytelen értéket adott meg!");
+}
+
+
+function clearInput(){
+    const inputElement = document.querySelector("#num");
+    inputElement.value = "";
+    inputElement.focus();
+}
+
+
+const coloring = () => {
+    const button = document.querySelector(".card button:nth-child(3)");
+    button.addEventListener("click", () => {
+        coloringBox();
+        clearInput();
+    });
+}
+
+const reset = () =>{
+    const resetButton = document.querySelector(".card button:nth-child(4)")
+    resetButton.addEventListener("click", () => {
+        renderBoxes();
+    })
+}
 
 document.addEventListener("DOMContentLoaded", () => {
-    renderBoxes()
-    const boxes = document.querySelectorAll(".box");
-    console.log(boxes);
+    renderBoxes();
+    coloring();
+    reset();
+    const boxes = document.querySelectorAll(".box")
 });
 
 
